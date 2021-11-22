9273fad44aa7591af52658d6f71ff4f5c7235d6d
larissa.gremelmaier.rosa <larissa.gremelmaier.rosa@grad.ufsc.br>
2021-11-22 16:18:37 -0300

    interface grafica - v1

--- pai-sho/src/main/java/PaiSho.java
+++ pai-sho/src/main/java/PaiSho.java
@@ -1,12 +1,15 @@
 package main.java;
 
 import main.java.visual.InterfacePaiSho;
-
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.TransferHandler;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,7 +19,10 @@ public class PaiSho {
     public static final String resources_path = "pai-sho/src/main/resources/";
     public static final String game_name = "Pai-sho";
     public static final int board_size = 612; //Pixel Size
-
+    public static ImageIcon icon = new ImageIcon(resources_path + "dark-lotus-tile.png");
+    public static Image image = icon.getImage(); // transform it
+    public static Image newimg = image.getScaledInstance(20, 20,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+    public static ImageIcon icon2 = new ImageIcon(newimg);
     public static void main(String [] args) {
         int matrix_size = 17;
         var imageIcon = new ImageIcon(resources_path + "iroh.jpg");
@@ -32,24 +38,77 @@ public class PaiSho {
 
                 frame.setIconImage(imageIcon.getImage());
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationByPlatform(true);
+                frame.setLocationRelativeTo(null);
 
 //
 //                String connection_adress = JOptionPane.showInputDialog(frame,
 //                        "Enter server adress or cancel to host a new game", null);
 
                 try {
+                    var containerPane = new JPanel();
+                    containerPane.setLayout(new GridBagLayout());
+
                     var painel_interface = new InterfacePaiSho();
                     JButton add_piece_btn = new JButton("Adicionar uma peça");
+                    add_piece_btn.addMouseListener(new MouseAdapter(){
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            JButton piece = new JButton();
+                            piece.setIcon(icon2);
+                            piece.addMouseListener(new MouseAdapter() {
+                                @Override
+                                public void mouseClicked(MouseEvent e) {
+                                    System.out.println("foi");
+                                    painel_interface.getButton((JButton) e.getComponent());
+                                }
+                            });
+                            painel_interface.add(piece, 3);
+                            piece.setBounds(301, 55, 20, 20);
+
+                        }
+                    });
                     JLabel turn_info = new JLabel("É seu turno");
-
+                    var data_interface = new  JPanel();
                     JButton forfeit_btn = new JButton("Desistir");
+                    add_piece_btn.addMouseListener(new MouseAdapter(){
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            turn_info.setText("Você desistiu");
+                        }
+                    });
+                    JButton change_player = new JButton("Passar Turno");
+                    change_player.addMouseListener(new MouseAdapter(){
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            turn_info.setText("Turno do oponente");
+                        }
+                    });
+                    JButton btn_start = new JButton("Iniciar");
+                    btn_start.addMouseListener(new MouseAdapter(){
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            turn_info.setText("O jogo começou");
+                        }
+                    });
+                    data_interface.add(turn_info);
+                    data_interface.add(add_piece_btn);
+                    data_interface.add(forfeit_btn);
+                    data_interface.add(btn_start);
+                    data_interface.add(change_player);
 
+                    GridBagConstraints c = new GridBagConstraints();
+                    c.fill = GridBagConstraints.BOTH;
+                    c.weightx = 1;
+                    c.gridy = 0;
+                    c.weighty = 0.1;
+                    containerPane.add(data_interface, c);
+                    c.weighty = 1;
+                    c.gridy = 1;
+                    c.gridheight = 3;
+                    containerPane.add(painel_interface, c);
+                    frame.add(containerPane);
 
-                    painel_interface.add(turn_info);
-                    painel_interface.add(add_piece_btn);
-                    painel_interface.add(forfeit_btn);
-
-                    frame.add(painel_interface);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- pai-sho/src/main/java/visual/InterfacePaiSho.java
+++ pai-sho/src/main/java/visual/InterfacePaiSho.java
@@ -8,38 +8,132 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import static main.java.PaiSho.resources_path;
 
-public class InterfacePaiSho extends JPanel {
+public class InterfacePaiSho extends JLayeredPane {
 
 
     private BufferedImage background;
-
+    JButton button_to_change = null;
     public InterfacePaiSho() throws IOException {
         File pathToFile = new File(resources_path + "board.png");
-        BufferedImage board = ImageIO.read(pathToFile);
-        background = board;
+        Image board = ImageIO.read(pathToFile).getScaledInstance(612, 612, Image.SCALE_SMOOTH);
+        BufferedImage buffered = new BufferedImage(612, 612, BufferedImage.TYPE_INT_ARGB);
+        buffered.getGraphics().drawImage(board, 0, 0 , null);
 
+        background = buffered;
+        setLayout(null);
+
+        setSize(getPreferredSize());
         File pathToFile2 = new File(resources_path + "dark-lotus-tile.png");
         BufferedImage tile = ImageIO.read(pathToFile2);
+        ImageIcon icon = new ImageIcon(resources_path + "dark-lotus-tile.png");
+        Image image = icon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(20, 20,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        ImageIcon icon2 = new ImageIcon(newimg);
+        int k = 9;
+        for (int i = 0; i < 5; i++){
+            Font font1 = new Font("SansSerif", Font.BOLD, 20);
+            int aux = 0;
+            for (int j = k; j>0; j--){
+                JLabel point = new JLabel();
+                point.setText(".");
+                point.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if (button_to_change == null){
+                            System.out.println("não há peça para mover");
+                        }else{
+                            System.out.println("há peça para mover");
+                            button_to_change.setBounds(point.getX(), point.getY(), 20, 20);
+                            button_to_change = null;
+                        }
+                    }
+                });
+                point.setFont(font1);
+                add(point, 2);
 
-        this.addMouseListener(new MouseAdapter(){
-            @Override
-            public void mousePressed(MouseEvent e) {
-                Graphics2D g2d = background.createGraphics();
-                g2d.setColor(Color.BLACK);
-                g2d.drawImage(tile, e.getX()-10, e.getY()-100-10, 20, 20, null, null);
-                g2d.dispose();
-                repaint();
+                if (j == k){
+                    aux = getWidth()/2 - k*30/2;
+                } else { aux = aux + 30;}
+                point.setBounds(aux, 25+i*30, 20, 20);
             }
-        });
+            k= k + 2;
+        }
+        k = 19;
+        for (int i = 0; i < 9; i++){
+            Font font1 = new Font("SansSerif", Font.BOLD, 20);
+            int aux = 0;
+            for (int j = k; j>0; j--){
+                JLabel point = new JLabel();
+                point.setText(".");
+                point.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if (button_to_change == null){
+                            System.out.println("não há peça para mover");
+                        }else{
+                            System.out.println("há peça para mover");
+                            button_to_change.setBounds(point.getX(), point.getY(), 20, 20);
+                            button_to_change = null;
+                        }
+                    }
+                });
+                point.setFont(font1);
+                add(point, 2);
+
+                if (j == k){
+                    aux = getWidth()/2 - k*30/2;
+                } else { aux = aux + 30;}
+                point.setBounds(aux, 175+i*30, 20, 20);
+            }
+
+        }
+
+        k= 17;
+        for (int i = 0; i < 6; i++){
+            Font font1 = new Font("SansSerif", Font.BOLD, 20);
+            int aux = 0;
+            for (int j = k; j>0; j--){
+                JLabel point = new JLabel();
+                point.setText(".");
+                point.setFont(font1);
+                point.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if (button_to_change == null){
+                            System.out.println("não há peça para mover");
+                        }else{
+                            System.out.println("há peça para mover");
+                            button_to_change.setBounds(point.getX(), point.getY(), 20, 20);
+                            button_to_change = null;
+                        }
+                    }
+                });
+                add(point, 2);
+
+                if (j == k){
+                    aux = getWidth()/2 - k*30/2;
+                } else { aux = aux + 30;}
+                point.setBounds(aux, 445+i*30, 20, 20);
+            }
+            k = k - 2;
+        }
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(612, 812);
+        return new Dimension(812, 812);
     }
+
+    public void getButton(JButton button){
+        button_to_change = button;
+    }
+
 
     @Override
     protected void paintComponent(Graphics g) {
